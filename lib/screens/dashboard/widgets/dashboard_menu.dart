2709142d941777e59422ccbf9d3e7ae39e9586a6
EliasTormos <elias.tormos@ceff.ch>
2023-06-06 16:16:35 +0200

    getLockerByFloor + dashboard

--- lib/screens/dashboard/widgets/dashboard_menu.dart
+++ lib/screens/dashboard/widgets/dashboard_menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lockers_app/models/locker.dart';
+import 'package:lockers_app/models/student.dart';
 import 'package:lockers_app/providers/lockers_student_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -11,6 +13,18 @@ class DashboardMenu extends StatefulWidget {
 
 class _DashboardMenuState extends State<DashboardMenu> {
   bool isFocus = false;
+  List<Locker> lockers = [];
+  List<Student> student = [];
+
+  @override
+  void initState() {
+    lockers =
+        Provider.of<LockerStudentProvider>(context, listen: false).lockerItems;
+    student =
+        Provider.of<LockerStudentProvider>(context, listen: false).studentItems;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -49,44 +63,52 @@ class _DashboardMenuState extends State<DashboardMenu> {
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          ...Provider.of<LockerStudentProvider>(context)
-                              .studentItems
-                              .map(
-                                (student) => MouseRegion(
-                                  onEnter: (event) => setState(() {
-                                    student.isFocus = true;
-                                  }),
-                                  onExit: (event) => setState(() {
-                                    student.isFocus = false;
-                                  }),
-                                  child: ListTile(
-                                    title: Text(
-                                        "${student.firstName} ${student.lastName}"),
-                                    trailing: Visibility(
-                                      visible: student.isFocus,
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          IconButton(
-                                            onPressed: () {},
-                                            icon: const Icon(
-                                              Icons.check,
-                                              color: Colors.black54,
-                                            ),
-                                          ),
-                                          IconButton(
-                                            onPressed: () {},
-                                            icon: const Icon(
-                                              Icons.close,
-                                              color: Colors.black54,
-                                            ),
-                                          )
-                                        ],
+                          ...student.map(
+                            (student) => MouseRegion(
+                              onEnter: (event) => setState(() {
+                                student.isFocus = true;
+                              }),
+                              onExit: (event) => setState(() {
+                                student.isFocus = false;
+                              }),
+                              child: ListTile(
+                                title: Text(
+                                  "${student.firstName} ${student.lastName}",
+                                  style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                subtitle: Text(
+                                  student.job,
+                                  style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                trailing: Visibility(
+                                  visible: student.isFocus,
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          Icons.check,
+                                          color: Colors.black54,
+                                        ),
                                       ),
-                                    ),
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          Icons.close,
+                                          color: Colors.black54,
+                                        ),
+                                      )
+                                    ],
                                   ),
                                 ),
                               ),
+                            ),
+                          ),
                         ],
                       ),
                     ),
@@ -115,54 +137,55 @@ class _DashboardMenuState extends State<DashboardMenu> {
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          ...Provider.of<LockerStudentProvider>(context)
-                              .lockerItems
-                              .map(
-                                (locker) => MouseRegion(
-                                  onEnter: (event) => setState(() {
-                                    locker.isFocus = true;
-                                  }),
-                                  onExit: (event) => setState(() {
-                                    locker.isFocus = false;
-                                  }),
-                                  child: ListTile(
-                                    title: Text(
-                                      "Casier n°${locker.lockerNumber} (Étage ${locker.floor.toUpperCase()})",
-                                    ),
-                                    subtitle: Text(
-                                      locker.remark == ''
-                                          ? 'Aucune remarque'
-                                          : locker.remark.toString(),
-                                      style: const TextStyle(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w500,
+                          ...lockers.map(
+                            (locker) => MouseRegion(
+                              onEnter: (event) => setState(() {
+                                locker.isFocus = true;
+                              }),
+                              onExit: (event) => setState(() {
+                                locker.isFocus = false;
+                              }),
+                              child: ListTile(
+                                title: Text(
+                                  "Casier n°${locker.lockerNumber} (Étage ${locker.floor.toUpperCase()})",
+                                  style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                subtitle: Text(
+                                  locker.remark == ''
+                                      ? 'Aucune remarque'
+                                      : locker.remark.toString(),
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                trailing: Visibility(
+                                  visible: locker.isFocus,
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          Icons.check,
+                                          color: Colors.black54,
+                                        ),
                                       ),
-                                    ),
-                                    trailing: Visibility(
-                                      visible: locker.isFocus,
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          IconButton(
-                                            onPressed: () {},
-                                            icon: const Icon(
-                                              Icons.check,
-                                              color: Colors.black54,
-                                            ),
-                                          ),
-                                          IconButton(
-                                            onPressed: () {},
-                                            icon: const Icon(
-                                              Icons.close,
-                                              color: Colors.black54,
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    ),
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          Icons.close,
+                                          color: Colors.black54,
+                                        ),
+                                      )
+                                    ],
                                   ),
                                 ),
                               ),
+                            ),
+                          ),
                         ],
                       ),
                     ),
