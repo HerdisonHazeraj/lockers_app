cf499d859ab7a014ee0c6633f60f31b323ce2004
TimoPortal <89443537+TimoPortal@users.noreply.github.com>
2023-08-22 13:48:39 +0200

    elias2

--- lib/screens/assignation/menu_widgets/actionbar_widget.dart
+++ lib/screens/assignation/menu_widgets/actionbar_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:lockers_app/screens/assignation/menu_widgets/dropdown_item_widget.dart';
 import 'package:lockers_app/screens/assignation/menu_widgets/sort_element_widget.dart';
-import 'package:lockers_app/screens/assignation/widgets/filter_element.dart';
+import 'package:lockers_app/screens/assignation/menu_widgets/filter_element.dart';
 import 'package:lockers_app/screens/core/widgets/divider_menu.dart';
 import '../../../models/locker.dart';
 import '../../../models/student.dart';
