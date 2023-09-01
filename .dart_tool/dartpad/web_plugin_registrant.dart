fb645a8d75ec7278f3457b6c48d5dccdf53f6be3
Herdison Hazeraj <Herdison.Hazeraj@ceff.ch>
2023-09-01 09:37:51 +0200

    Système de login terminé

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -7,6 +7,7 @@
 // ignore_for_file: type=lint
 
 import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_database_web/firebase_database_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
@@ -16,6 +17,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   FilePickerWeb.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FirebaseDatabaseWeb.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
